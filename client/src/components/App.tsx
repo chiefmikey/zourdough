33047c34790c1f144182438a36451fd69451a0d2
chiefmikey <wolfemikl@gmail.com>
2022-02-17 11:14:54 -0700

    Include home content

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import Footer from './Footer';
+import Home from './Home';
+import Nav from './Nav';
+
 const App = () => {
-  return <div>Hello World</div>;
+  return (
+    <div className="app-container">
+      <div className="logo">
+        <img
+          src="/public/assets/img/zourdough-logo.png"
+          alt="Zourdough Sourdough"
+        />
+      </div>
+      <Nav />
+      <Home />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
