15010c48ec7681dea35713ea1c101b33d4ec29b3
chiefmikey <wolfemikl@gmail.com>
2022-02-17 11:15:17 -0700

    Create content index

---
+++ client/src/content/contentIndex.ts
@@ -0,0 +1,17 @@
+import navButtons from './header/navButtons';
+import instructionsButtons from './instructions/instructionsButtons';
+import instructionsContent from './instructions/instructionsContent';
+import sliderGalleryContent from './slider/sliderGalleryContent';
+
+const contentIndex = {
+  // Header navigation buttons
+  navButtons,
+  // Instructions + recipes page links
+  instructionsButtons,
+  // Instructions + recipes posts content
+  instructionsContent,
+  // Home page slider gallery content
+  sliderGalleryContent,
+};
+
+export default contentIndex;
