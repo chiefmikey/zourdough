913bff550f9367e85d99015320473c1992bbf160
chiefmikey <wolfemikl@gmail.com>
2022-02-17 11:15:09 -0700

    Include slider gallery

---
+++ client/src/components/SliderGallery.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import contentIndex from '../content/contentIndex';
+
+const SliderGallery = () =>
+  contentIndex.sliderGalleryContent.map((sliderCard) => {
+    return (
+      <div key={sliderCard.title} className="home-slider-card">
+        <a href={sliderCard.path}>
+          <div className="home-slider-card-image">
+            <img src={sliderCard.image} alt="Slider Gallery Card" />
+          </div>
+          {sliderCard.title}
+        </a>
+      </div>
+    );
+  });
+
+export default SliderGallery;
