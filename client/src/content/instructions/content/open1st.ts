8efebd51ea47ca6e8dfcb54702295999411dc5f3
chiefmikey <wolfemikl@gmail.com>
2022-02-17 11:15:28 -0700

    First instructions post content

---
+++ client/src/content/instructions/content/open1st.ts
@@ -0,0 +1,103 @@
+const content = `
+So: here is a copy of what came with your starter and what to do after day 5 and on:>
+
+so just putting on the counter is perfectly perfect.
+
+But if your house is really cold, find a warm place to put the starter.
+
+We also mention pure water,,,,thats just our humor:>...all water is good unless it smells.
+
+Remember this is supposed to be fun
+
+chris
+
+
+NOTE: your starter is NEVER dead, email me first if you think you killed it, or if you "think" it is not alive.(REALLY IT TAKES A FLAME THROWER TO KILL MY BABIES!!!)  OUR starters are very active and may need 2 feedings daily even by the fourth day.
+
+
+An active starter eats up all its food and goes back to sleep, at this point it looks like smelly water on top (hooch), IS LIQUIDY and is just doing nothing, IT IS NOT DEAD, just give it a good feeding and stir it up, it awakes when you feed it again..never throw it out!!
+
+
+oh and dont get caught up in the type of flour, my babies eat anything, and the type of water is ok too, unless you smell chemicals.
+
+
+
+In adding water we say 1/2 cup, but what your looking for is a VERY VERY thick but stirable consistency, so maybe not a full 1/2 cup.  You decide.
+
+
+NOTE: WE USE ALL PURPOSE FLOUR, BUT ADDING A LITTLE RYE FLOUR, WHOLE WHEAT FLOUR, OR BETTER FOR BREAD FLOUR HELPS ALOT, ¼ cup or more, ESPECIALLY IF YOUR NOT SEEING IT RISE, THE ADDED GLUTEN WILL TAKE CARE OF THAT.
+
+
+
+
+
+ACTIVATION INSTRUCTIONS
+
+
+First off, everywhere I read it says do not use metal bowls or utensils, well no long term contact, spoons for mixing are fine and so is the mixing bowl
+
+its a metal and acid thing….the sourdough has an acidic base to it..
+
+
+ok now then:
+
+Mix 1/2 cup flour, 1 tablespoon sugar (this time only) and  ABOUT 1/2 cup  lukewarm water, and package of starter powder in a plastic bowl OR GLASS BOWL.  Cover bowl WITH PLASTIC WRAP OR TOWEL and put on the counter
+
+On the second day just stir it up.
+
+On the third day add an additional 1/2 cup  flour and ABOUT 1/2 cup of lukewarm water OR LESS TO MAKE THICK VERY THICK.
+
+On the fourth day add about 1/2 cup lukewarm water and 1/2 cup  flour, we now want the starter very very thick but stirrable.
+
+ THIS IS THE DAY EVERYONE WRITES ME AND SAYS, MY STARTER IS LOOKING SAD AND LIQUIDY!  WELL THATS THE SIGN OF A VERY VERY ACTIVE STARTER AND DAY 5 WILL TAKE CARE OF THAT:>ALSO ADDING THAT RYE , WHOLE WHEAT OR BETTER FOR BREAD FLOUR WILL ADD MORE GLUTEN AND FIX ALOT OF THINGS FOR YOU:> HOW MUCH?  YOU DECIDE, ¼ CUP IS GOOD..
+
+At day five YOU WILL  REMOVE ½ OF THE STARTER AND MAKE PANCAKES WITH THAT, (SEE PANCAKE FILE)
+
+DAMN IT JUST DO IT, DO IT NOW!!!OOPS YOU MISSED DAY 5 TAKE OFF ½ INSTUCTIONS, ITS ALL GOOD,,JUST DO IT TODAY AND FEED THE REST 1 FULL CUP OF FLOUR AND ENOUGH WATER TO MAKE VERY VERY THICK,
+
+ AFTER THAT, DAY 6 YOUR READY TO GO, FEED DAILY OR put it in the refrigerator and feed it at least once monthly.  How much to feed is now up to you!
+
+At day seven you may continue to keep your starter at room temperature feeding it daily or put it in the refrigerator and feed it at least once weekly IF USING IT, OR ONCE MONTHLY IF NOT USING IT.  SAY YOU WANT TO USE IT THE NEXT DAY, TAKE IT OUT THE NIGHT BEFORE AND FEED IT, THE YEASTIES WILL BE READY TO GO TO WORK FOR YOU  IN THE MORNING.
+
+
+ Whats that you say, your bowl is going to overflow, pour some out you dummy. Really, do I have to
+
+spell it all out for you, use common sense:>
+
+
+WAIT A MINUTE: TAKE A 1/2 CUP OF STARTER AND PUT IT IN SOMETHING LIKE A
+
+QUART JAR, PUNCH HOLES IN THE TOP SO IT DOESN'T BLOW UP, GAS IS A BYPRODUCT,  add a 1/2 cup of
+
+flour and some water to it and put in the fridge. This is your BACK-UP starter, the one you will start all
+
+other batches with, IF YOU HAPPEN TO KILL OR USE ALL YOUR WORKING STARTER,  feed every now and then to keep it alive.
+
+
+NOTE: IF USING THE OVEN FOR A WARM PLACE, hang a red scarf on the oven and tell everyone that your baby is in there (don't put a
+
+real baby in the oven, you will go to jail and it makes them taste funny) Learned this the hard
+
+way, not the real baby taste thing sheesh, the sour dough starter in the bowl thing. Seems my
+
+wife forgot and preheated the oven without looking inside, which is normal to do, even though
+
+I accused her of trying to destroy my hobby, well, a melted plastic bowl, almost lost my starter
+
+along with a big mess in the oven was the result. What did we learn? Hang a red scarf on the
+
+oven handle, although she did it again a few months later and the scarf was in place. The battle
+
+between my wife and I over this hobby is just another story.
+
+Cheers and great bread
+
+Christopher
+`;
+
+const open1st = {
+  title: 'Open 1st - New',
+  content,
+};
+
+export default open1st;
